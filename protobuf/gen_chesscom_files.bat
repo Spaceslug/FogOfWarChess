bdc225fb149f6c5a8b61196d6faa2cb4ee7080a1
Spaceslug <hovard93@gmail.com>
2019-08-29 18:25:45 +0200

    .bat fix

--- protobuf/gen_chesscom_files.bat
+++ protobuf/gen_chesscom_files.bat
@@ -1,4 +1,4 @@
-
+if not exist "csharp\" mkdir "csharp\"
 
 protoc.exe chesscom.proto --grpc_out=csharp --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe --proto_path=.\
 protoc.exe chesscom.proto --csharp_out=csharp --proto_path=.\
